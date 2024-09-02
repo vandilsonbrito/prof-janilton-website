0391536018bf6cc235c1b20b7a710c14ce84b77e
vandilsonbrito <vandilson25@outlook.com>
2024-09-02 11:41:24 -0300

    Feat: add header, banner, convencimento, sobre, articleCTASection, canal, gallery, contato and footer components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
-import Image from "next/image";
+import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
+import Convencimento from "./components/WinningSection/WinningSection";
+import Sobre from './components/AboutSection/AboutSection';
+import Canal from './components/ChannelSection/ChannelSection';
+import Contato from './components/ContactSection/ContactSection';
+import Footer from "./components/Footer/Footer";
+import Gallery from "./components/GallerySection/Gallery";
+import ArticleCTASection from "./components/ArticleCTASection/ArticleCTASection";
 
 export default function Home() {
   return (
-    <main className="w-full h-full">
-      <h1>Prof. Janilton</h1>
+    <main className="w-full h-full ">
+        {/* <Helmet>
+          <title>Professor Dr. Janilton Almeida. Aprenda a dar aulas criativas.</title>
+          <meta name="description" content="O Professor Janilton é um especialista na arte de dar aulas criativas, pesquisa educacional e inovação. É um professor de Geografia com mais de vinte e cinco anos de atuação com Mestrado em Ensino de Ciências e Doutorado em Ciências."/>
+          <link rel="canonical" href="https://profjanilton.com.br" />
+        </Helmet> */}
+        <Header/>
+        <div className="overflow-hidden flex flex-col justify-center items-center">
+          <Banner/>
+          <Convencimento/>
+          <Sobre/>
+          <ArticleCTASection/>
+          <Canal/>
+          <Gallery/>
+          <Contato/>
+          <Footer/>
+        </div>
     </main>
-  );
+  )
 }
