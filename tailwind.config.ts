1605950b49a1178108cea30324c6684e81919d2f
vandilsonbrito <vandilson25@outlook.com>
2024-09-01 23:51:22 -0300

    Chore: add hero-image, about-image and define 'main-blue' color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'hero-image': "url('../public/hero-image.jpg')",
+        'about-image': "url('../public/about-pic.jpg')"
+      },
+      colors: {
+        'main-blue': '#2b2da1'
       },
     },
   },
