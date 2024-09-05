bc0a38269753f504adcc40305f2fe6b5c1551013
vandilsonbrito <vandilson25@outlook.com>
2024-09-05 13:12:45 -0300

    Feat: add metadata and dynamic loading for components

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,67 @@ import Contato from './components/ContactSection/ContactSection';
 import Footer from "./components/Footer/Footer";
 import Gallery from "./components/GallerySection/Gallery";
 import ArticleCTASection from "./components/ArticleCTASection/ArticleCTASection";
+import dynamic from 'next/dynamic';
+import Head from "next/head";
+import { Metadata } from "next";
+import Banner from "./components/Banner/Banner";
+
+
+export const metadata: Metadata = {
+    title: 'Professor Dr. Janilton de Lima Almeida.',
+    description: 'O Professor Janilton é um especialista na arte de dar aulas criativas, pesquisa educacional e inovação. É um professor de Geografia com mais de vinte e cinco anos de atuação com Mestrado em Ensino de Ciências e Doutorado em Ciências.',
+    alternates: {
+      canonical: 'https://profjanilton.com.br',
+    },
+    openGraph: {
+      title: 'Professor Dr. Janilton de Lima Almeida - Professor de Geografia e Iniciação Científica.',
+      description: 'O Professor Janilton é um especialista na arte de dar aulas criativas, pesquisa educacional e inovação.',
+      images: [
+        {
+          url: 'https://asset.cloudinary.com/ds7cszkkx/434e2c7590e8a9d4038bed857aed81bc',
+          alt: 'Professor Dr. Janilton de Lima Almeida',
+          width: 400,
+          height: 250
+        }
+      ],
+      url: 'https://profjanilton.com.br/',
+      type: 'website',
+    },
+    
+};
+
 
 export default function Home() {
-  return (
-    <main className="w-full h-full ">
-        {/* <Helmet>
-          <title>Professor Dr. Janilton Almeida. Aprenda a dar aulas criativas.</title>
-          <meta name="description" content="O Professor Janilton é um especialista na arte de dar aulas criativas, pesquisa educacional e inovação. É um professor de Geografia com mais de vinte e cinco anos de atuação com Mestrado em Ensino de Ciências e Doutorado em Ciências."/>
-          <link rel="canonical" href="https://profjanilton.com.br" />
-        </Helmet> */}
-        <Header/>
-        <div className="overflow-hidden flex flex-col justify-center items-center">
-          <Banner/>
-          <Convencimento/>
-          <Sobre/>
-          <ArticleCTASection/>
-          <Canal/>
-          <Gallery/>
-          <Contato/>
-          <Footer/>
-        </div>
-    </main>
-  )
+
+    
+    const Convencimento = dynamic(() => import('./components/ConvinceSection/ConvinceSection'), {
+      ssr: true, 
+      loading: () => <p>Loading...</p>
+    });
+    const Contato = dynamic(() => import('./components/ContactSection/ContactSection'), {
+      ssr: true, 
+      loading: () => <p>Loading...</p>
+    });
+    const Gallery = dynamic(() => import('./components/GallerySection/Gallery'), {
+      ssr: false, // Desativa SSR se o componente não for necessário para SEO
+      loading: () => <p>Loading...</p>
+    });
+    
+
+    return (
+      <main className="w-full h-full ">
+
+          <Header/>
+          <div className="overflow-hidden flex flex-col justify-center items-center">
+            <Banner/>
+            <Convencimento/>
+            <Sobre/>
+            <ArticleCTASection/>
+            <Canal/>
+            <Gallery/>
+            <Contato/>
+            <Footer/>
+          </div>
+      </main>
+    )
 }
