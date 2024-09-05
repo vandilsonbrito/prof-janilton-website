d87b51cabf148035f603b86d90b61d2562370af6
vandilsonbrito <vandilson25@outlook.com>
2024-09-05 13:01:35 -0300

    Feat: create interfaces file for articles data

---
+++ utils/interafces.ts
@@ -0,0 +1,20 @@
+export interface simpleBlogCard {
+    title: string,
+    smallDescription: string,
+    currentSlug: string,
+    titleImage: any
+}
+
+export interface articleData {
+    currentSlug: string,
+    title: string,
+    smallDescription: string
+    titleImage: any
+}
+export interface fullBlogData {
+    currentSlug: string,
+    title: string,
+    smallDescription: string
+    content: any,
+    titleImage: any
+}
